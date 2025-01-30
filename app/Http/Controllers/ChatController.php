2d51ec6fe01c76dc949f4984ab8f0c78ab212d30
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-29 18:40:47 -0600

    Add constructor to ChatController for OpenAIService dependency injection and define chat route

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
+use App\Services\OpenAIService;
 
 class ChatController extends Controller
 {
-    //
+    protected $OpenAIService;
+
+    public function __construct(OpenAIService $OpenAIService)
+    {
+        $this->OpenAIService = $OpenAIService;
+    }
 }
