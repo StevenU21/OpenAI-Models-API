dabe52b2a9d3b1da3cff96a9ce735f979f7f5073
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-30 12:10:40 -0600

    Add Pusher configuration options to .env.example and update broadcasting settings

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -12,3 +12,13 @@
 //     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
 //     enabledTransports: ['ws', 'wss'],
 // });
+
+// or
+// window.Pusher = Pusher;
+
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+//     forceTLS: true
+// });
