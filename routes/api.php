5ec501318dd26c3c46b73c28db785133c2ebe3ff
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-30 01:43:52 -0600

    Refactor route names for conversation and streamed conversation in Chat API

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,6 @@ Route::get('/user', function (Request $request) {
 Route::prefix('chat')->name('chat.')->group(function () {
     Route::get('/models', [ChatController::class, 'getModels'])->name('models');
     Route::get('/prompts', [ChatController::class, 'getPrompts'])->name('prompts');
-    Route::post('/', [ChatController::class, 'conversation'])->name('store');
-    Route::post('/streamed', [ChatController::class, 'streamed_conversation_sse'])->name('streamed');
+    Route::post('/', [ChatController::class, 'conversation'])->name('conversation');
+    Route::post('/streamed', [ChatController::class, 'streamed_conversation_sse'])->name('streamed.conversation');
 });
