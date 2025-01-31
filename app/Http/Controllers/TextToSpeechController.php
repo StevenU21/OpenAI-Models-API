336f83b8d69916aeef30bba3815302d784c0925b
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-30 22:39:23 -0600

    Add methods to retrieve speech languages and response formats in TextToSpeechController

--- app/Http/Controllers/TextToSpeechController.php
+++ app/Http/Controllers/TextToSpeechController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Services\OpenAIService;
+use Illuminate\Http\JsonResponse;
 
 class TextToSpeechController extends Controller
 {
@@ -13,18 +14,30 @@ class TextToSpeechController extends Controller
         $this->OpenAIService = $OpenAIService;
     }
 
-    public function getTextToSpeechModels()
+    public function getTextToSpeechModels(): JsonResponse
     {
         return $this->OpenAIService->getTextToSpeechModels();
     }
 
-    public function getSpeechVoices()
+    public function getSpeechVoices(): JsonResponse
     {
         return $this->OpenAIService->getSpeechVoices();
     }
 
-    public function getSpeechVoicesAudio()
+    public function getSpeechVoicesAudio(): JsonResponse
     {
         return $this->OpenAIService->getSpeechVoiceAudios();
     }
+
+    public function getSpeechLanguages(): JsonResponse
+    {
+        return $this->OpenAIService->getSpeechLanguages();
+    }
+
+    public function getSpeechResponseFormats(): JsonResponse
+    {
+        return $this->OpenAIService->getSpeechResponseFormats();
+    }
+
+
 }
